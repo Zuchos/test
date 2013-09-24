6422937ba2e72301fbe79bd7316a1b3230fb5b85
zuchos <lukasz.zuchowski@gmail.com>
2013-09-24 14:50:11 +0200

    Test List<String>

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+        List<String> test = new ArrayList<String>();
+        System.out.println(test);
+    }
+}
